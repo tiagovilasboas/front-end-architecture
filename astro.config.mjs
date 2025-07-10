adb55433f491fe76362dafa6f9097577e57313f2
Tiago Montanha <tcarvalhovb@gmail.com>
2025-07-10 12:25:25 -0300

    feat: add sitemap and dynamic SEO meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://arc.tiagovilasboas.dev',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()],
+
+  markdown: {
+    syntaxHighlight: 'prism',
+  }
 });
